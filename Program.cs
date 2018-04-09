f3572cc85266ec1187e27cb44b579ad1f5dd4b99
WuXiaoFeng <38064693+XiaoFengWu1988@users.noreply.github.com>
2018-04-09 10:52:05 +0800

    Create Program.cs
    program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication
+{
+   class Program
+   {
+    //用来标注此方法为单线程
+    [STAThread]
+    static void main()
+    {
+
+    }
+   }
+}
